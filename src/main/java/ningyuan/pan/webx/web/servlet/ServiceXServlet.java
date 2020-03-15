08ca262ab92aa562d42dd50a32b534930fc272a1
Ningyuan Pan <pan.neo@hotmail.com>
2020-03-15 22:09:36 +0800

    Integrate ServicX module

---
+++ src/main/java/ningyuan/pan/webx/web/servlet/ServiceXServlet.java
@@ -0,0 +1,58 @@
+/**
+ * 
+ */
+package ningyuan.pan.webx.web.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import ningyuan.pan.servicex.impl.ServiceXImpl;
+import ningyuan.pan.servicex.service.ServiceX;
+
+/**
+ * @author ningyuan
+ *
+ */
+//@WebServlet("/servicex")
+public class ServiceXServlet extends HttpServlet {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(ServiceXServlet.class);
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		LOGGER.debug("doGet()");
+		
+		response.setCharacterEncoding("UTF-8");
+        
+		response.setContentType("text/html;charset=utf-8");
+        
+        ServiceX service = new ServiceXImpl();
+        
+        PrintWriter out = response.getWriter();
+        
+        out.write(service.getName());
+        out.close();
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		LOGGER.debug("doPost()");
+		
+		doGet(request, response);
+	}
+
+}
