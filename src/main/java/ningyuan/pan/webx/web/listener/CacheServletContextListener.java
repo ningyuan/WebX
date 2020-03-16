1af10c932f8bac7a0b176989629dd3cf3b907bfd
Ningyuan Pan <pan.neo@hotmail.com>
2020-03-16 23:45:39 +0800

    Add read property file

--- src/main/java/ningyuan/pan/webx/web/listener/CacheServletContextListener.java
+++ src/main/java/ningyuan/pan/webx/web/listener/CacheServletContextListener.java
@@ -3,6 +3,10 @@
  */
 package ningyuan.pan.webx.web.listener;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Properties;
+
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
@@ -10,8 +14,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import ningyuan.pan.webx.util.cache.Cache;
-import ningyuan.pan.webx.util.cache.redis.RedisCache;
-import ningyuan.pan.webx.util.cache.redis.RedisCache1;
+import ningyuan.pan.webx.util.cache.redis.JedisCache;
 
 /**
  * @author ningyuan
@@ -26,12 +29,24 @@ public class CacheServletContextListener implements ServletContextListener {
 		LOGGER.debug("contextInitialized()");
 		
 		String cacheName = sce.getServletContext().getInitParameter("cacheName");
+		String configPropertyFile = sce.getServletContext().getInitParameter("configPropertyFile");
 		
-		Cache cache = new RedisCache1();
+		Properties configProp;
+		try {
+			configProp = new Properties();
+        	configProp.load(new InputStreamReader(getClass().getClassLoader().getResourceAsStream(configPropertyFile)));
+		} catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
 		
-		cache.open();
-		
-		sce.getServletContext().setAttribute(cacheName, cache);
+		if(cacheName.equals("RedisCache")) {
+			Cache cache = new JedisCache();
+	    	
+			cache.open(configProp.getProperty("redis.host"), configProp.getProperty("redis.port"));
+	    		
+			sce.getServletContext().setAttribute(cacheName, cache);
+		}
 	}
 
 	@Override
@@ -40,7 +55,8 @@ public class CacheServletContextListener implements ServletContextListener {
 		
 		Cache cache = (Cache)sce.getServletContext().getAttribute(sce.getServletContext().getInitParameter("cacheName"));
 		
-		cache.close();
+		if(cache != null) {
+			cache.close();
+		}
 	}
-
 }
