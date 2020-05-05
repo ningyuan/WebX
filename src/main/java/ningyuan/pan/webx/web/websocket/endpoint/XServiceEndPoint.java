f14a401725b84834a2f1fb556829766e4904a143
Ningyuan Pan <pan.neo@hotmail.com>
2020-05-05 23:45:04 +0800

    Add websocket end point

--- src/main/java/ningyuan/pan/webx/web/websocket/endpoint/XServiceEndPoint.java
+++ src/main/java/ningyuan/pan/webx/web/websocket/endpoint/XServiceEndPoint.java
@@ -3,38 +3,84 @@
  */
 package ningyuan.pan.webx.web.websocket.endpoint;
 
+
+import java.io.IOException;
+
 import javax.websocket.CloseReason;
-import javax.websocket.Endpoint;
 import javax.websocket.EndpointConfig;
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
 import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import ningyuan.pan.util.exception.ExceptionUtils;
 
 /**
  * @author ningyuan
  *
  */
-public class XServiceEndPoint extends Endpoint{
-	
+@ServerEndpoint("/websocket/xservice")
+public class XServiceEndPoint {
 	private static final Logger LOGGER = LoggerFactory.getLogger(XServiceEndPoint.class);
 	
-	@Override
-	public void onOpen(Session session, EndpointConfig config) {
-		
+	private static final String MESSAGE_THREAD = "mt";
+	
+	@OnOpen
+	public void open(Session session, EndpointConfig conf) { 
+		LOGGER.debug("open()");
 	}
 	
-	@Override
-	public void onClose(Session session, CloseReason closeReason) {
-		// TODO Auto-generated method stub
-		super.onClose(session, closeReason);
+	@OnMessage
+	public void message(Session session, String msg) { 
+		LOGGER.debug("message() "+msg);
+		
+		Thread mt = (Thread)session.getUserProperties().get(MESSAGE_THREAD);
+		
+		if(mt == null || !mt.isAlive()) {
+			mt = new Thread(new Runnable() {
+				
+				@Override
+				public void run() {
+					while(!Thread.currentThread().isInterrupted()) {
+						try {
+							LOGGER.debug(msg);
+							session.getBasicRemote().sendText("echo "+msg);
+							Thread.sleep(1000);
+						}
+						catch (IOException ioe) {
+							LOGGER.debug(ExceptionUtils.printStackTraceToString(ioe));
+							break;
+						} catch (InterruptedException ie) {
+							break;
+						}
+					}
+				}
+			});
+			
+			session.getUserProperties().put(MESSAGE_THREAD, mt);
+			mt.start();
+		}
 	}
-
-	@Override
-	public void onError(Session session, Throwable thr) {
-		// TODO Auto-generated method stub
-		super.onError(session, thr);
+	
+	@OnError
+	public void error(Session session, Throwable error) { 
+		LOGGER.debug("error()");
 	}
-
+	
+	@OnClose
+	public void close(Session session, CloseReason reason) {
+		LOGGER.debug("close()");
+		Thread mt = (Thread)session.getUserProperties().get(MESSAGE_THREAD);
+		
+		if(mt != null) {
+			mt.interrupt();
+		}
+		
+		session.getUserProperties().remove(MESSAGE_THREAD);
+	}
 }
