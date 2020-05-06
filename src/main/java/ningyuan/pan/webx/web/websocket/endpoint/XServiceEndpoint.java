bf0e662d4534d898e70656716bcadb4e722bc1f7
Ningyuan Pan <pan.neo@hotmail.com>
2020-05-06 13:33:24 +0800

    Modify XServiceEndpoint

---
+++ src/main/java/ningyuan/pan/webx/web/websocket/endpoint/XServiceEndpoint.java
@@ -0,0 +1,88 @@
+/**
+ * 
+ */
+package ningyuan.pan.webx.web.websocket.endpoint;
+
+import java.io.IOException;
+
+import javax.websocket.CloseReason;
+import javax.websocket.EndpointConfig;
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import ningyuan.pan.util.exception.ExceptionUtils;
+
+/**
+ * @author ningyuan
+ *
+ */
+@ServerEndpoint("/websocket/xservice")
+public class XServiceEndpoint {
+	private static final Logger LOGGER = LoggerFactory.getLogger(XServiceEndpoint.class);
+	
+	private static final String MESSAGE_THREAD = "mt";
+	
+	@OnOpen
+	public void open(Session session, EndpointConfig conf) { 
+		LOGGER.debug("open()");
+		
+		Thread mt = (Thread)session.getUserProperties().get(MESSAGE_THREAD);
+		
+		if(mt == null) {
+			mt = new Thread(new Runnable() {
+				
+				@Override
+				public void run() {
+					while(!Thread.currentThread().isInterrupted()) {
+						try {
+							String num = String.valueOf(Math.round((Math.random()*100)));
+							session.getBasicRemote().sendText(num);
+							
+							LOGGER.trace(num);
+							
+							Thread.sleep(1000);
+						}
+						catch (IOException ioe) {
+							LOGGER.debug(ExceptionUtils.printStackTraceToString(ioe));
+							break;
+						} catch (InterruptedException ie) {
+							break;
+						}
+					}
+				}
+			});
+			
+			session.getUserProperties().put(MESSAGE_THREAD, mt);
+			mt.start();
+		}
+	}
+	
+	@OnMessage
+	public void message(Session session, String msg) { 
+		LOGGER.debug("message() "+msg);
+	}
+	
+	@OnError
+	public void error(Session session, Throwable error) { 
+		LOGGER.debug("error()");
+	}
+	
+	@OnClose
+	public void close(Session session, CloseReason reason) {
+		LOGGER.debug("close()");
+		Thread mt = (Thread)session.getUserProperties().get(MESSAGE_THREAD);
+		
+		if(mt != null) {
+			mt.interrupt();
+		}
+		
+		session.getUserProperties().remove(MESSAGE_THREAD);
+	}
+}
