7de4aed469ff0d2391cb1ae13dd61d3a65df2af9
Ningyuan Pan <pan.neo@hotmail.com>
2020-05-16 16:30:03 +0800

    Integrate JTA with Atomikos

--- src/main/java/ningyuan/pan/webx/web/listener/ServiceServletContextListener.java
+++ src/main/java/ningyuan/pan/webx/web/listener/ServiceServletContextListener.java
@@ -5,6 +5,9 @@ package ningyuan.pan.webx.web.listener;
 
 import java.sql.Connection;
 
+import javax.jms.Session;
+import javax.persistence.EntityManager;
+
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
@@ -15,13 +18,18 @@ import org.slf4j.LoggerFactory;
 import ningyuan.pan.servicex.XService;
 import ningyuan.pan.servicex.impl.XServiceDAOImpl;
 import ningyuan.pan.servicex.impl.XServiceMybatisImpl;
+import ningyuan.pan.servicex.persistence.dao.RoleDAO;
 import ningyuan.pan.servicex.persistence.dao.UserDAO;
+import ningyuan.pan.servicex.persistence.dao.impl.RoleDAOJPAImpl;
 import ningyuan.pan.servicex.persistence.dao.impl.UserDAOJDBCImpl;
+import ningyuan.pan.servicex.persistence.dao.impl.UserDAOJPAImpl;
 import ningyuan.pan.servicex.util.GlobalObjectName;
 import ningyuan.pan.servicex.util.ServiceXUtil;
 import ningyuan.pan.util.exception.ExceptionUtils;
+import ningyuan.pan.util.persistence.ActiveMQXADataSourceManager;
 import ningyuan.pan.util.persistence.DataSourceManager;
 import ningyuan.pan.util.persistence.JDBCDataSourceManager;
+import ningyuan.pan.util.persistence.JPADataSourceManager;
 import ningyuan.pan.util.persistence.MybatisDataSourceManager;
 import ningyuan.pan.webx.util.ServiceName;
 import ningyuan.pan.webx.web.servlet.XServiceServlet;
@@ -37,33 +45,43 @@ public class ServiceServletContextListener implements ServletContextListener {
 	
 	@Override
 	public void contextInitialized(ServletContextEvent sce) {
-		DataSourceManager<SqlSession> dataSourceManager = null;
+		//DataSourceManager<SqlSession> dataSourceManager = null;
 		//DataSourceManager<Connection> dataSourceManager = null;
+		DataSourceManager<EntityManager> XADataSourcemanager = null;
+		DataSourceManager<Session> JMSXADataSourceManager = null;
 		
 		try {
-			dataSourceManager = new MybatisDataSourceManager();
+			//dataSourceManager = new MybatisDataSourceManager();
 			//dataSourceManager = new JDBCDataSourceManager();
+			XADataSourcemanager = new JPADataSourceManager("dev-jta-hibernate");
+			JMSXADataSourceManager = new ActiveMQXADataSourceManager();
 			
 			//UserDAO userDAO = dataSourceManager.initAndGetThreadLocalConnection().getMapper(UserDAO.class);
+			UserDAO userDAO = new UserDAOJPAImpl(XADataSourcemanager);
+			RoleDAO roleDAO = new RoleDAOJPAImpl(XADataSourcemanager);
 			
-			XService serviceX = new XServiceMybatisImpl();
-			//XService serviceX = new XServiceJDBCImpl(userDAO);
+			//XService serviceX = new XServiceMybatisImpl();
+			//XService serviceX = new XServiceJDBCImpl(userDAO, roleDAO);
+			XService serviceX = new XServiceDAOImpl(userDAO, roleDAO);
 			
-			ServiceXUtil.getInstance().setGelobalObject(GlobalObjectName.MYBATIS_DATA_SOURCE_MANAGER, dataSourceManager);
+			//ServiceXUtil.getInstance().setGelobalObject(GlobalObjectName.MYBATIS_DATA_SOURCE_MANAGER, dataSourceManager);
 			//ServiceXUtil.getInstance().setGelobalObject(GlobalObjectName.JDBC_DATA_SOURCE_MANAGER, dataSourceManager);
+			ServiceXUtil.getInstance().setGelobalObject(GlobalObjectName.XA_DATA_SOURCE_MANAGER, XADataSourcemanager);
+			ServiceXUtil.getInstance().setGelobalObject(GlobalObjectName.JMS_XA_DATA_SOURCE_MANAGER, JMSXADataSourceManager);
 			
+			System.out.println("!!!"+ServiceName.X_SERVICE.getName());
 			sce.getServletContext().setAttribute(ServiceName.X_SERVICE.getName(), serviceX);
 		}
 		catch(Exception e) {
 			LOGGER.debug(ExceptionUtils.printStackTraceToString(e));
 			
-			if(dataSourceManager != null) {
+			/*if(dataSourceManager != null) {
 				try {
 					dataSourceManager.close();
 				} catch (Exception e1) {
 					LOGGER.debug(ExceptionUtils.printStackTraceToString(e1));
 				}
-			}
+			}*/
 			
 			ServiceXUtil.getInstance().removeGelobalObject(GlobalObjectName.MYBATIS_DATA_SOURCE_MANAGER);
 			sce.getServletContext().removeAttribute(ServiceName.X_SERVICE.getName());
@@ -73,18 +91,40 @@ public class ServiceServletContextListener implements ServletContextListener {
 	@Override
 	public void contextDestroyed(ServletContextEvent sce) {
 		@SuppressWarnings("unchecked")
-		DataSourceManager<SqlSession> dataSourceManager = (DataSourceManager<SqlSession>)ServiceXUtil.getInstance().getGelobalObject(GlobalObjectName.MYBATIS_DATA_SOURCE_MANAGER);
+		//DataSourceManager<SqlSession> dataSourceManager = (DataSourceManager<SqlSession>)ServiceXUtil.getInstance().getGelobalObject(GlobalObjectName.MYBATIS_DATA_SOURCE_MANAGER);
 		//DataSourceManager<Connection> dataSourceManager = (DataSourceManager<Connection>)ServiceXUtil.getInstance().getGelobalObject(GlobalObjectName.JDBC_DATA_SOURCE_MANAGER);
+		DataSourceManager<EntityManager> XADataSourcemanager = (DataSourceManager<EntityManager>)ServiceXUtil.getInstance().getGelobalObject(GlobalObjectName.XA_DATA_SOURCE_MANAGER);
+		DataSourceManager<Session> JMSXADataSourceManager = (DataSourceManager<Session>)ServiceXUtil.getInstance().getGelobalObject(GlobalObjectName.JMS_XA_DATA_SOURCE_MANAGER);
 		
-		if(dataSourceManager != null) {
+		
+		/*if(dataSourceManager != null) {
 			try {
 				dataSourceManager.close();
 			} catch (Exception e) {
 				LOGGER.debug(ExceptionUtils.printStackTraceToString(e));
 			}
+		}*/
+		
+		if(XADataSourcemanager != null) {
+			try {
+				XADataSourcemanager.close();
+			} catch (Exception e) {
+				LOGGER.debug(ExceptionUtils.printStackTraceToString(e));
+			}
 		}
 		
-		ServiceXUtil.getInstance().removeGelobalObject(GlobalObjectName.MYBATIS_DATA_SOURCE_MANAGER);
+		if(JMSXADataSourceManager != null) {
+			try {
+				JMSXADataSourceManager.close();
+			} catch (Exception e) {
+				LOGGER.debug(ExceptionUtils.printStackTraceToString(e));
+			}
+		}
+		
+		//ServiceXUtil.getInstance().removeGelobalObject(GlobalObjectName.MYBATIS_DATA_SOURCE_MANAGER);
+		ServiceXUtil.getInstance().removeGelobalObject(GlobalObjectName.XA_DATA_SOURCE_MANAGER);
+		ServiceXUtil.getInstance().removeGelobalObject(GlobalObjectName.JMS_XA_DATA_SOURCE_MANAGER);
+		
 		sce.getServletContext().removeAttribute(ServiceName.X_SERVICE.getName());
 	}
 
--- src/main/java/ningyuan/pan/webx/web/servlet/StartPageServlet.java
+++ src/main/java/ningyuan/pan/webx/web/servlet/StartPageServlet.java
@@ -5,13 +5,20 @@ package ningyuan.pan.webx.web.servlet;
 
 import java.io.IOException;
 
+import javax.annotation.Resource;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import ningyuan.pan.util.exception.ExceptionUtils;
 
 
 /**
@@ -28,6 +35,11 @@ public class StartPageServlet extends HttpServlet {
 	 */
 	private static final long serialVersionUID = 1L;
 	
+	/*
+	 * @Resource(lookup = "java:/comp/env", name = "jdbc/XAdatasource")
+	 * private DataSource dataSource;
+	 */
+	
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		LOGGER.debug("doGet()");
@@ -37,7 +49,18 @@ public class StartPageServlet extends HttpServlet {
 		LOGGER.debug(request.getRequestURL().toString());
 		LOGGER.debug(request.getRequestURI());
 		
-	
+		Context initContext;
+		try {
+			initContext = new InitialContext();
+			Context envContext = (Context) initContext.lookup("java:/comp/env");  
+			DataSource dataSource = (DataSource)envContext.lookup("jdbc/XAdatasource"); 
+			
+			LOGGER.debug(dataSource.toString());
+		} catch (NamingException e) {
+			ExceptionUtils.printStackTraceToString(e);
+		}  
+		
+		
 		// path starting with '/' is interpreted as relative to current context root
 		request.getRequestDispatcher("/pages/start.html").forward(request, response);
 	}
